2c083738ae19ff13479a4fae9889b9f4aa88df96
GIECOM <GIECOM>
2017-02-15 19:38:41 -0500

    primer commit

---
+++ Sección 2 - Introducion a Visual Studios/4. Primer Programa - Hola Mami/PrimerHolaMundo/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1 'con forme se crean modulos se deben cerrar
+
+    Sub Main() 'se debe crear un sub lllamado Main para que le programa sepa dond es el inicio
+
+
+        Console.WriteLine("Hola Mundo") 'con éste comando podemos imprimir por pantalla
+        Console.Read() 'el read se usa para tomar datos del usuario pero en éste caso es solo para que se no se cierre la ventana
+
+
+    End Sub
+
+End Module
